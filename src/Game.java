51299770cf8fc942a9b30a97a08a6ed69fd29bb1
gylese <Emily@Emilys-MacBook-Pro.local>
2015-06-21 16:42:36 -0400

    Changed printlni n loop and car color

--- src/Game.java
+++ src/Game.java
@@ -7,7 +7,6 @@ import java.awt.event.*;
 public class Game
 {
     public boolean start;
-
     public Road road;
     public int here;
 
@@ -17,8 +16,8 @@ public class Game
     public String player1Name;
     public static String playerTwoName;
     public String player2Name;
-    public static int car1;
-    public static int car2;
+    public static int car1Color;
+    public static int car2Color;
     private int screenWidth;
     private int screenHeight;
     public Game()
@@ -43,13 +42,16 @@ public class Game
         start = false;
         
         while(!start){
-        	System.out.println();
+        	try{
+        		Thread.sleep(5);
+        	}catch(Exception e){
+        	}
         }
         test.remove(mainScreen);
         test.setVisible(false);
+        
         playerOneName = JOptionPane.showInputDialog(null,"Player 1: Enter Name","Car 1", JOptionPane.PLAIN_MESSAGE);
         player1Name = playerOneName;
-        
         //Shortcut
       if((player1Name.equals("buggy")))
         {
@@ -61,18 +63,20 @@ public class Game
       
       //Displays JOptionPanes to ask for names/ difficulties
         Object[] levels = {"Extremely Difficult", "Medium", "Easy"};
+        Object[] carColors = {"Red", "Blue", "Green"};
+        car1Color = JOptionPane.showOptionDialog(null, "What color car would you like?", "Car 1 Color", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, carColors, carColors[0]);
         difficulty1 = JOptionPane.showOptionDialog(null, "Player 1: Choose your Difficulty", "Car 1",JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,null, levels, levels[0]);
-        
         playerTwoName = JOptionPane.showInputDialog(null,"Player 2: Enter Name","Car 2", JOptionPane.PLAIN_MESSAGE);
         player2Name = playerTwoName;
+        car2Color = JOptionPane.showOptionDialog(null, "What color car would you like?", "Car 2 Color", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, carColors, carColors[0]);
         difficulty2 = JOptionPane.showOptionDialog(null, "Player 2: Choose your Difficulty", "Car 2",JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,null, levels, levels[0]);
-        
+
         //Only creates road once the last question has been answered
         if(difficulty2 != 3){
        
 
-        Car a = new Car(180*screenWidth/800, 670*screenHeight/800, player1Name);
-        Car b = new Car(580*screenWidth/800, 670*screenHeight/800, player2Name);
+        Car a = new Car(180*screenWidth/800, 670*screenHeight/800, player1Name, car1Color);
+        Car b = new Car(580*screenWidth/800, 670*screenHeight/800, player2Name, car2Color);
         road = new Road(a, b, this);
         road.start();
         test.setVisible(true);
